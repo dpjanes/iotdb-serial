18a216266ca981a55c7242f3f22b08ef7c08dbf7
David P. Janes <davidjanes@davidjanes.com>
2014-03-30 06:43:52 -0400

    - inputs and interrupts

--- server/comm_rpio.py
+++ server/comm_rpio.py
@@ -25,60 +25,49 @@ class CommRPIO(comm.Comm):
         self.board = 0
         self.listensd = {}
         self.modeld = {}
+        self.pind = {}
 
         self.send("!hello","CommRPIO")
 
+    def _setup_pin(self, pin, pin_type):
+        if self.pind.get(pin):
+            return
+
+        self.pind[pin] = True
+        RPIO.setup(pin, pin_type)
 
     def receive(self, sequence, command, *av):
         if command == "DigitalWrite":
             pin, value = av[0], av[1]
 
-            RPIO.setup(pin, RPIO.OUT)
+            self._setup_pin(pin, RPIO.OUT)
             RPIO.output(pin, value and True or False)
         elif command == "DigitalRead":
-            pass
-            ## pin = av[0]
+            pin = av[0]
+
+            self._setup_pin(pin, RPIO.IN)
+            return RPIO.input(pin) and 1 or 0
             ## return pifacedigitalio.digital_read(pin, self.board)
         elif command == "DigitalListen":
-            pass
-            """
             pin = av[0]
             self.listensd.setdefault(pin, set()).add(sequence)
 
-            if self.listener:
-                self.listener.deactivate()
-
-            self.listener = pifacedigitalio.InputEventListener(chip=self.pfd)
-
-            for pin in self.listensd.keys():
-                self.listener.register(pin, pifacedigitalio.IODIR_ON, self._event)
-                self.listener.register(pin, pifacedigitalio.IODIR_OFF, self._event)
-
-            self.listener.activate()
-            return ""
-            """
+            RPIO.add_interrupt_callback(pin, send._event)
         else:
             raise KeyError("unknown command: %s" % command)
             
         print ("comm_piface.receive: seq=%s comm=%s" % ( sequence, command ), file=sys.stderr)
 
-    """
     def stop(self):
-        if self.listener:
-            self.listener.deactivate()
-            self.listener = None
+        pass
 
-    def _event(self, event):
-        pin = event.pin_num
-
+    def _event(self, pin, value):
         listens = self.listensd.get(pin)
         if not listens:
             return
 
         for sequence in listens:
-            value = pifacedigitalio.digital_read(pin, self.board)
             self.send(sequence,value)
-    """
 
 if __name__ == '__main__':
     comm = CommRPIO(verbose=True)
